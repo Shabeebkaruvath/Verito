ebe56196e500a2e56df9d692bc39c490f4cde829
shabeeb <shabeebkaruvath@gmail.com>
2023-12-10 16:25:10 +0530

    php updated

--- contact.php
+++ contact.php
@@ -6,10 +6,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST['message'];
     $sub = $_POST['subject'];
 
+    // Validate email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format";
+        exit; // Stop further execution if email is invalid
+    }
+
+    // Sanitize inputs
+    $name = htmlspecialchars($name);
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($message);
+    $sub = htmlspecialchars($sub);
+
     // Email details
     $to = "shabeebkaruvath@gmail.com"; // Replace with your email
     $subject = "New Contact Form Submission";
-    $body = "Name: $name\nEmail: $email\nSubject:$sub\nMessage: $message";
+    $body = "Name: $name\nEmail: $email\nSubject: $sub\nMessage: $message";
 
     // Send email
     if (mail($to, $subject, $body)) {
